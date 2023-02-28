ba48a0844bcf8e3bf02fe1917d9a13419859979d
Arpon <a4arpon@gmail.com>
2023-02-28 17:03:00 +0600

    Git Init  Changes to be committed: 	new file:   .gitignore 	new file:   data.json 	new file:   home.js 	new file:   index.html 	new file:   key.js 	new file:   player.html

---
+++ home.js
@@ -0,0 +1,47 @@
+let data = {};
+const youtubeEngine = async (key, q) => {
+  try {
+    const response = await fetch(
+      `https://www.googleapis.com/youtube/v3/search?key=${key}&type=video&q=${q}&part=snippet`,
+      {
+        method: "GET",
+      }
+    );
+    data = await response.json(); // Use response.json() to parse the response body
+    printer(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+const searchHandaler = () => {
+  let searchQ = document.getElementById("search_box").value;
+  youtubeEngine(key, searchQ);
+};
+const printer = (data) => {
+  let filteredData = data.items;
+  let DOM = "";
+  for (let i = 0; i < filteredData.length; i++) {
+    DOM += `<div class="col-md-3 px-1 py-1">
+    <div class="card p-1">
+      <a href="./player.html?ytID=${filteredData[i].id.videoId}" class="text-decoration-none">
+        <img
+          src="${filteredData[i].snippet.thumbnails.medium.url}"
+          alt="..."
+          class="w-100"
+        />
+        <div class="card-body">
+          <p class="card-text text-white mb-2">
+            ${filteredData[i].snippet.title}
+          </p>
+          <a
+            href=""
+            class="text-white text-decoration-none badge bg-primary"
+            >${filteredData[i].snippet.channelTitle}</a
+          >
+        </div>
+      </a>
+    </div>
+  </div>`;
+  }
+  document.getElementById("trees").innerHTML = DOM;
+};
